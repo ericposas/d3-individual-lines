76a6e2cfab40ff95a17de1ba65e15ef4a41d2e1b
ericposas <web_dev@tutanota.com>
2020-08-05 23:24:18 -0500

    removes g elements and redraws a new random set of data

--- src/index.js
+++ src/index.js
@@ -4,50 +4,58 @@ import './style.scss'
 
 
 window.linetest = () => {
+
 	let svg = d3.select('svg')
 
-	let data = new Array(random.int(10, 50))
-	.fill(1)
-	.map(a => {
-		return {
-			x: random.int(1, 1000),
-			y: random.int(1, 1000)
-		}
-	})
-	.sort((a, b) => b.x - a.x)
+	const innerFunc = () => {
+		let data = new Array(random.int(10, 50))
+		.fill(1)
+		.map(a => {
+			return {
+				x: random.int(1, 1000),
+				y: random.int(1, 1000)
+			}
+		})
+		.sort((a, b) => b.x - a.x)
 
-	let scX = d3.scaleLinear()
-	.domain(d3.extent(data, d => d['x']))
-	.range([600, 0])
+		let scX = d3.scaleLinear()
+		.domain(d3.extent(data, d => d['x']))
+		.range([600, 0])
 
-	let scY = d3.scaleLinear()
-	.domain(d3.extent(data, d => d['y']))
-	.range([500, 0])
+		let scY = d3.scaleLinear()
+		.domain(d3.extent(data, d => d['y']))
+		.range([500, 0])
 
-	let g = svg.append('g')
+		let lastDataLen = 0
 
-	g
-	.selectAll('line')
-	.data(data).enter()
-	.append('line')
-	.attr('x1', (d, i) => i < 1 ? 0 : scX(data[i-1]['x']))
-	.attr('y1', (d, i) =>  i < 1 ? 0 : scY(data[i-1]['y']))
-	.attr('x2', d => scX(d['x']))
-	.attr('y2', d => scY(d['y']))
-	.transition().duration(1000).delay((d, i) => i * 50)
-	.attr('stroke', '#000')
-	.attr('stroke-width', 2)
+		if (svg.selectAll('g')) svg.selectAll('g').remove()
 
-	g
-	.selectAll('circle')
-	.data(data).enter()
-	.append('circle')
-	.attr('fill', 'green').attr('r', 1)
-	.attr('cx', d => scX(d['x']))
-	.attr('cy', d => scY(d['y']))
-	.transition().duration(1000).ease(d3.easeCubic).delay((d, i) => i * 25)
-	.attr('r', 5)
+		svg
+		.append('g')
+		.selectAll('line')
+		.data(data).enter().append('line')
+		.attr('x1', (d, i) => i < 1 ? 0 : scX(data[i-1]['x']))
+		.attr('y1', (d, i) =>  i < 1 ? 0 : scY(data[i-1]['y']))
+		.attr('x2', (d, i) => scX(data[i]['x']))
+		.attr('y2', (d, i) => scY(data[i]['y']))
+		.transition().duration(1000).delay((d, i) => i * 50)
+		.attr('stroke', '#000')
+		.attr('stroke-width', 2)
 
+		svg
+		.select('g')
+		.selectAll('circle')
+		.data(data).enter()
+		.append('circle')
+		.attr('fill', 'green').attr('r', 1)
+		.attr('cx', d => scX(d['x']))
+		.attr('cy', d => scY(d['y']))
+		.transition().duration(1000).ease(d3.easeCubic).delay((d, i) => i * 25)
+		.attr('r', 5)
+
+		setTimeout(innerFunc, 5000)
+	}
+	innerFunc()
 }
 
 window.ht = () => {
